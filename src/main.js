c10c5782151c3dc15cfe0d5522d55cddf7a7bad5
rocketdayo <rocket3141592653589@gmail.com>
2026-07-04 12:47:02 +0900

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // src/main.js
 // updated: 2026-07-04 (v0.1.1)
 
+import Phaser from "https://cdn.jsdelivr.net/npm/phaser@3.80.1/dist/phaser.esm.min.js";
 import GameScene from "./scenes/GameScene.js";
 
 const config = {
